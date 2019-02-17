e1f723fb6743bd1d594d25ac4aca7413826eb2bd
Mateus Machado <matteusmachhado@hotmail.com>
2019-02-17 00:53:28 -0300

    Add camada de segurança.

---
+++ 3-SRC/1-Domain/YS.CMS.Domain.Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YS.CMS.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
---
+++ 3-SRC/3-Infra/YS.CMS.Infra.Security/UserHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using YS.CMS.Domain.Entities;
+using YS.CMS.Infra.Security.Model;
+
+namespace YS.CMS.Infra.Security
+{
+    public class UserHandler
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserHandler(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<UserManagerResultModel> CreateUser(User model)
+        {
+            var user = new User { UserName = model.UserName };
+            var result = await _userManager.CreateAsync(user, model.PasswordHash);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return new UserManagerResultModel { Succeeded = true };
+            }
+            return new UserManagerResultModel { Succeeded = false };
+        }
+    }
+}
